45e2c2d875b5f33fba6c50e799698d61835154ec
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 02:45:25 -0700

    String Interpolations

--- dartprogramingtutorials/bin/dartprogramingtutorials.dart
+++ dartprogramingtutorials/bin/dartprogramingtutorials.dart
@@ -1,3 +1,30 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  //literals..literals means all Datatype before assignning to a variable they are called litterals
+  //4...
+  //'solomon'
+  //true
+  // all, 5, 'SOLOMON' AND True are called litterals
+
+  int age = 4; //age have int literals
+  String hisnames = "solomon girma"; // String Literals with Double quotes
+  String name = 'solomon girma'; // string literals with single quotes
+  String namess =
+      "it's name is solomon"; // double quotes is used inside double quotes;
+  String myname =
+      'it\'smy name'; // use single quote inside single quote with \ character'
+  //String interpolation means connecting large text as a single text.
+
+  String longString = "my name is soloon girma" + "i graduated from astu";
+
+  String longStringwithoutaddition = "my name is soloon girma"
+      "i graduated from astu";
+
+  //String Interpolation
+
+  String Myname = "solomon";
+  String message = "my name is " + Myname;
+  print(message);
+
+  //String interpolation
+  print("my name is $Myname");
 }
