6e4d0ba3f2b9ba8b491ba8b3807e261adc185be4
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 00:56:55 -0700

    Data types and variables in dart

--- dart.dart
+++ dart.dart
@@ -3,4 +3,11 @@
 // the is single line comment
 /* this 
 is double line comment */
-// in dart convention single line comment is best.
+// in dart convention single line comment is best./
+//in these section we will see
+//1, how to declare a variables
+//2, what are Built in Data Types in Dart
+//3, what is Literals in dart
+//4, string interpolations
+//4, Constant in Dart
+//6,  final  and const keyword
