902daf44fab1e8a925bdc25c9c2862536d02694b
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 07:50:03 -0700

    if else Statement

--- dartprogramingtutorials/bin/ifesle.dart
+++ dartprogramingtutorials/bin/ifesle.dart
@@ -3,8 +3,8 @@ void main() {
   //conditional
   //switch case statement
 
-  var mark = 70;
-  if (mark >= 90 && mark < 100) {
+  var mark = 59;
+  if (mark >= 90 && mark <= 100) {
     print("A+ grade");
   } else if (mark >= 80 && mark < 90) {
     print("B+ Grade");
