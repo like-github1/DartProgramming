47e86411babfa723de11b02bc3063ed9e1210c6c
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 01:10:00 -0700

    there are 8 data type in dart

--- dart.dart
+++ dart.dart
@@ -1,13 +1,15 @@
-// Comment in dart there are two types of comment in dart programing languges
-// single line comment and double line comment
-// the is single line comment
-/* this 
-is double line comment */
-// in dart convention single line comment is best./
-//in these section we will see
+
 //1, how to declare a variables
 //2, what are Built in Data Types in Dart
 //3, what is Literals in dart
 //4, string interpolations
 //4, Constant in Dart
 //6,  final  and const keyword
+
+//Daart Data typess
+//1,Numbers
+//2, String
+//3, Booleans
+//List(array)
+// Map, Runes and Symbols..the are 8 data types in dart
+//Number(only int and double there is no flot), string, bolean, map,List,Runes and symbols
