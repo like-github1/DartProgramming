7384369fb8527505e49a4d16613c5986c4dc0df0
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 09:21:12 -0700

    conditional Statement

--- dartprogramingtutorials/bin/ifesle.dart
+++ dartprogramingtutorials/bin/ifesle.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 void main() {
   //if else
   //conditional
@@ -15,4 +17,41 @@ void main() {
   } else {
     print("your grade is f");
   }
+  //Conditional Expression
+//Syntax
+//Condition1v? exp1:exp2;
+//exp1?? exp2;
+//examples
+  int a = 12;
+  int b = 4;
+  if (a < b) {
+    print('a is smaller');
+  } else {
+    print("b is smaller");
+  }
+
+  //Conditional Satement
+  //forexamples (Conditions)? exp1:exp2;
+  a < b
+      ? print(' we can see that $a is smaller')
+      : print('we can see that $b  is smaller');
+  //condition??exp1
+  //exp1??exp2;
+  int age = 34;
+  int age1 = 35;
+  int smallerNumber;
+  if (age < age1) {
+    smallerNumber = age;
+  } else {
+    smallerNumber = age1;
+  }
+  print('the smaller Number is $smallerNumber');
+//a < b ? print("$a is smaller than b ") : print('$b is smaller than a');
+  int mysmaller = a < b ? a : b;
+  print('the smaller number is $mysmaller');
+  var fname;
+  String nametoprint = fname ?? "guestuser";
+  print(nametoprint);
+
+  //
 }
