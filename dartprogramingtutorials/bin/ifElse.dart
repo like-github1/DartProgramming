ed1e10c01daa834b7b0d6727aa88c9913d80c200
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 07:45:31 -0700

    Conditional Statement

--- dartprogramingtutorials/bin/ifElse.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  //in these tutorials we will learn Dart Control Flow Satatement
-  //if-else
-  //Conditional- Sateent
-  //sWITCH_CASE_satetement
-}
---
+++ dartprogramingtutorials/bin/ifesle.dart
@@ -0,0 +1,18 @@
+void main() {
+  //if else
+  //conditional
+  //switch case statement
+
+  var mark = 70;
+  if (mark >= 90 && mark < 100) {
+    print("A+ grade");
+  } else if (mark >= 80 && mark < 90) {
+    print("B+ Grade");
+  } else if (mark >= 70 && mark < 80) {
+    print('B- grade');
+  } else if (mark >= 60 && mark < 70) {
+    print("c grade");
+  } else {
+    print("your grade is f");
+  }
+}
