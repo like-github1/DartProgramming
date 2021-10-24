4c78d914a8372eeb212a37bb3f30eda9f812282a
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 01:32:08 -0700

    Create variableDeclaretion.dart
    if variable is decalred as a var. its data type depends on the value we assign. if we assign int value it's data type become int....

---
+++ variableDeclaretion.dart
@@ -0,0 +1,26 @@
+void main() {
+  //Two ways of Declaring variable as an intiger
+  int age = 10; //age variable is declare as intiger
+  var ages = 10; // here also ages variable are declare as intiger
+
+  //there are two ways of declaring Double variables
+  //ways to declare a variable as Double
+
+  double number = 22.3;
+  var numbers = 22.3;// it automatically infered as a double by the compiler.
+
+  //there are two way to Declare variable as intiger
+
+  var name = 'solomon';
+  var myname = 'solomon'; //it automatically infered as String by the compiler
+
+  //There are two ways to declare boolean variable
+
+  bool isvalid = true;
+  var isvalide = true; //it is infered as boolean automatically by compiler
+
+  //Note if you Declare a variable as Var, it's datatype is known after intialize it
+  
+
+
+}
