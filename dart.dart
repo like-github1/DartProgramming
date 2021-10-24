b0e0f8f548fef680a991e1954d284b728f5ec87b
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-24 00:23:40 -0700

    to print string ,number, and boolean

--- dart.dart
+++ dart.dart
@@ -4,4 +4,14 @@ void main() {
   print('hello students');
   //this prints hello students in console window
   print("this is my first applications");
+//we can print String "this is string "
+//we can print number examples 2
+//or we can print boolean value example, True or False
+//to print String
+  print("this is string");
+  //to print number 4
+  print(3);
+  //to print boolean we can use
+  print(true);
+  print(false);
 }
