bd569f5c1e49e90bb89d6a3d096d0a5192bd82dd
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-10-28 02:25:37 -0700

    exeption handling

--- dartprogramingtutorials/bin/exeption.dart
+++ dartprogramingtutorials/bin/exeption.dart
@@ -1,33 +1,11 @@
 void main() {
-  print("case 1");
+  //Exeption Handling in Dart
+  //try_catch_on_finally.
+
   try {
-    int number = 12 ~/ 0;
-    print('the result is $number');
+    int number = 23 ~/ 0;
+    print(number);
   } on IntegerDivisionByZeroException {
-    print("can not be divide byzero");
-  }
-  //when you do not know the name of Exeption use Catch close.
-  //if you are  sure about the exeption use on close
-  print("case 2");
-  try {
-    int number = 12 ~/ 0;
-    print('the result is $number');
-  } catch (e) {
-    print("the exception thrown is $e");
-  }
-  //stack trace what is happening before printing the Exeption,
-  print("case 2");
-  try {
-    int number = 12 ~/ 0;
-    print('the result is $number');
-  } catch (e, s) {
-    print("the exception thrown is $e");
-    print('the Stack trace will be $s');
-  }
-   try {
-    int number = 12 ~/ 0;
-    print('the result is $number');
-  } catch (e) {
-    print("the exception thrown is $e");
+    print("do not dividiblre by zero");
   }
 }
